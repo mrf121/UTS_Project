bf5336e492f694e52a5b278bb1aabe7f67d26d8a
mrf121 <45751598+mrf121@users.noreply.github.com>
2019-05-19 02:09:51 +0700

    Update OnboardingActivity.java

--- app/src/main/java/com/example/latihan3prauts/activity/OnboardingActivity.java
+++ app/src/main/java/com/example/latihan3prauts/activity/OnboardingActivity.java
@@ -1,5 +1,5 @@
 package com.example.latihan3prauts.activity;
-
+//17-05-2019  NIM : 10116344 Nama : R Muhammad Restu Fauzi KELAS : IF8
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
